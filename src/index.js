845f2d49400514782b2449d26487ea1997ac1e1e
Rasul <76545962+Rasulyo@users.noreply.github.com>
2021-03-23 15:54:54 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './index.less';
 
 const carousel = document.querySelector('.carousel');
 const carouselImages = document.querySelectorAll('.carousel-item img');
+const f_circle = document.querySelector('.first_circle')
+const s_circle = document.querySelector('.second_circle')
+const t_circle = document.querySelector('.third_circle')
 
 
 const nextBtn = document.querySelector('#nextBtn');
@@ -19,7 +22,7 @@ nextBtn.addEventListener('click', () => {
     counter++;
     console.log(counter)
     carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
+    
 })
 
 prevBtn.addEventListener('click', () => {
@@ -30,3 +33,21 @@ prevBtn.addEventListener('click', () => {
     counter--;
     carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
 })
+
+function goToFirst(){
+    carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    return counter = 0
+}
+
+f_circle.addEventListener('click', goToFirst)
+
+s_circle.addEventListener('click', () => {
+    carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    return counter = 1
+
+})
+t_circle.addEventListener('click', () => {
+    carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    return counter = 2
+})
+    
